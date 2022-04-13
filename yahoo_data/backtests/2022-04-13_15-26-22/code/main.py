fe639a20f05c63a138c6bfda57e1b2a418959cac
silardi <stephenjilardi@gmail.com>
2022-04-13 15:36:05 -0400

    first commit

---
+++ yahoo_data/backtests/2022-04-13_15-26-22/code/main.py
@@ -0,0 +1,28 @@
+from AlgorithmImports import *
+# from yahoo_reader import YahooData
+
+
+class Yahoodata(QCAlgorithm):
+    def Initialize(self):
+        self.SetStartDate(2021, 1, 1)  # Set Start Date
+        self.SetEndDate(2022, 1, 1)  # Set End Date
+        self.SetCash(100000)  # Set Strategy Cash
+
+        # self.AddEquity("SPY", Resolution.Minute)
+        self.symbol = self.AddData(YahooData, "UDOW", Resolution.Daily).Symbol
+
+    def OnData(self, data):
+        """OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
+            Arguments:
+                data: Slice object keyed by symbol containing the stock data
+        """
+        # if not self.Portfolio.Invested:
+        #     self.SetHoldings("UDOW", 1)
+        #     self.Debug("Purchased Stock")
+
+        # Keep track of the values
+        # self.Debug(f"{self.symbol.Value} - {self.Time}: Close={data[self.symbol].Close}")
+
+        close = self.Securities["UDOW"].Close
+        self.StopMarketOrder("UDOW", 10, round((close * .90), 2))
+        self.Debug(f"{self.Portfolio.TotalPortfolioValue}")
